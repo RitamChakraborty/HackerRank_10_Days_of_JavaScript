b1f1098339b86ffed2eec51b5f83938172f6f86c
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2020-01-01 21:51:17 +0530

    Solve Loops

---
+++ Day 2: Loops/main.js
@@ -0,0 +1,56 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the vowelsAndConsonants function.
+ * Print your output using 'console.log()'.
+ */
+function vowelsAndConsonants(s) {
+    let vowels = Array();
+    let consonants = Array();
+
+    for (let i in s) {
+        switch (s.charAt(i)) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                vowels.push(s.charAt(i));
+                break;
+            default:
+                consonants.push(s.charAt(i));
+        }
+    }
+
+    vowels.forEach(i => console.log(i));
+    consonants.forEach(i => console.log(i));
+}
+
+
+function main() {
+    const s = readLine();
+
+    vowelsAndConsonants(s);
+}
