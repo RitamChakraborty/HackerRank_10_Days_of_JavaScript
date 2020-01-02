53163d9e7ce60e893c101e3c4c29055c733fc0fe
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2020-01-02 08:32:48 +0530

    Solve Classes

---
+++ Day 4: Classes/main.js
@@ -0,0 +1,19 @@
+function Polygon(sides) {
+    this.sides = sides;
+    this.perimeter = () => {
+        let per = 0;
+
+        sides.forEach(i => per += i);
+
+        return per;
+    }
+}
+
+
+const rectangle = new Polygon([10, 20, 10, 20]);
+const square = new Polygon([10, 10, 10, 10]);
+const pentagon = new Polygon([10, 20, 30, 40, 43]);
+
+console.log(rectangle.perimeter());
+console.log(square.perimeter());
+console.log(pentagon.perimeter());
--- Day 4: Count Objects/main.js
+++ Day 4: Count Objects/main.js
@@ -32,7 +32,7 @@ function getCount(objects) {
     let count = 0;
 
     objects.forEach(i => {
-        if (i.x == i.y) {
+        if (i.x === i.y) {
             ++count;
         }
     });
