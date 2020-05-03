ed8e974412a2b16d681a7b7ef443184e3594338b
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2020-05-03 21:02:39 +0530

    Solve Day 8: Create a Button

---
+++ Day 8: Create a Button/js/button.js
@@ -0,0 +1,14 @@
+var count = 0;
+main();
+
+function main() {
+    const btn = document.getElementById("btn");
+    btn.innerHTML = count;
+    btn.addEventListener("click", incrementCounter(btn, count));
+}
+
+function incrementCounter(btn) {
+    return () => {
+        btn.innerHTML = ++count;
+    }
+}
