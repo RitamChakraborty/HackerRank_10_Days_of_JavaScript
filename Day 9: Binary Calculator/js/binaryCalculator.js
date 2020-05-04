604a0b3e9c516d7f455f67c1747f5aa1c5f1cb58
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2020-05-04 12:42:16 +0530

    Solve Day 9: Binary Calculator

---
+++ Day 9: Binary Calculator/js/binaryCalculator.js
@@ -0,0 +1,62 @@
+const btn0 = document.getElementById("btn0");
+const btn1 = document.getElementById("btn1");
+const btnClr = document.getElementById("btnClr");
+const btnEql = document.getElementById("btnEql");
+const btnSum = document.getElementById("btnSum");
+const btnSub = document.getElementById("btnSub");
+const btnMul = document.getElementById("btnMul");
+const btnDiv = document.getElementById("btnDiv");
+
+const res = document.getElementById("res");
+
+function addToRes(str) {
+    return () => {
+        res.innerHTML = res.innerHTML + str;
+    };
+}
+
+btn0.addEventListener("click", addToRes("0"));
+
+btn1.addEventListener("click", addToRes("1"));
+
+btnSum.addEventListener("click", addToRes("+"));
+
+btnSub.addEventListener("click", addToRes("-"));
+
+btnMul.addEventListener("click", addToRes("*"));
+
+btnDiv.addEventListener("click", addToRes("/"));
+
+btnClr.addEventListener("click", () => {
+    res.innerHTML = "";
+});
+
+btnEql.addEventListener("click", () => {
+    const statement = res.innerHTML;
+    var nums = statement.split(/(\+|-|\*|\/)/g);
+
+    if (nums.length === 3) {
+        const num1 = parseInt(nums[0], 2);
+        const num2 = parseInt(nums[2], 2);
+        const opt = nums[1];
+        let result;
+
+        switch (opt) {
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
+                result = num1 / num2;
+                break;
+        }
+
+        res.innerHTML = result.toString(2);
+    }
+});
+
